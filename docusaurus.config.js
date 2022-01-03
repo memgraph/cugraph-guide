c1c9007bb7b32365cd4d505b6dd7c22a5ccb4f83
Ivan Despot <ivan.g.despot@outlook.com>
2022-01-03 09:04:52 +0100

    Add GitHub Action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'cuGraph Guide',
+  tagline: 'GPU graph analytics with RAPIDS and NVIDIA',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'memgraph', // Usually your GitHub org/user name.
+  projectName: 'cugraph-guide', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
